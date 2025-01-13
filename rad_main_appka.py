0efdbc9411ad9ef1d9ed970395632d4468678b75
AndreiosR <orada@cmgp.cz>
2025-01-13 15:38:39 +0100

    Update rad_main_appka.py

--- rad_main_appka.py
+++ rad_main_appka.py
@@ -8,6 +8,16 @@ def prepocet_CML(weight,activityfactor):
     cmlrounded= round(cml, 1)
     return cmlrounded
 
+def mesto_backend(mesto):
+    mesto=text_a
+    if mesto == "Brno":
+        text_a="Pokračujte v moravském jazyce"
+    elif mesto == "Wien":
+        text_a="Geht ihr weiter in deutsche Sprache, bitte!"
+    elif mesto == "Bratislava":
+        text_a="Slováci rozumí moravsky..."
+    return text_a
+
 
 # Main Window Variables
 todays_date_str = dt.date.today().strftime("%d-%m-%Y") #this is a string
